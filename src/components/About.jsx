ad8435c20093e374ef74886b271fdabd9dd1812b
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-08-11 02:52:09 +0200

    stile About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,24 +5,25 @@ import typescript from "../assets/images/typesctipt.png";
 import react from "../assets/images/react.png";
 import redux from "../assets/images/redux.png";
 import spring from "../assets/images/spring.png";
+import postgreSQL from "../assets/images/postgreSQL.png";
 import { Container, Row, Col } from "react-bootstrap";
 
 const About = () => {
   return (
-    <Container className="d-flex flex-md-column flex-lg-row">
+    <Container className="d-flex flex-md-column flex-lg-row justify-content-center align-items-center">
       <Row>
         <Col>
           <h1>About</h1>
           <p className="spaziatura">
             Determined, solution oriented, passionate about nature and
             innovation, and driven by curiosity. <br /> I firmly believe in
-            technology's potential to drive positive change, <br />
-            and my mission is to create a meaningful impact on both the planet
-            and society.
+            technology's potential to drive positive change, <br /> and my
+            mission is to create a meaningful impact on both the planet and
+            society.
           </p>
         </Col>
       </Row>
-      <Row className=" mx-5 d-flex flex-row justify-content-center my-5">
+      <Row className="mx-5 d-flex flex-row justify-content-center my-5">
         <Col md={3} lg={3} className="iconeSkills">
           <img src={html5} alt="logo html5" />
         </Col>
@@ -36,13 +37,16 @@ const About = () => {
           <img src={typescript} alt="logo typescript" />
         </Col>
         <Col md={3} lg={3} className="iconeSkills">
-          <img src={react} alt="logo react" />{" "}
+          <img src={react} alt="logo react" />
         </Col>
         <Col md={3} lg={3} className="iconeSkills">
           <img src={redux} alt="logo redux" />
         </Col>
         <Col md={3} lg={3} className="iconeSkills">
           <img src={spring} alt="logo spring boot" />
+        </Col>
+        <Col md={3} lg={3} className="iconeSkills">
+          <img src={postgreSQL} alt="logo postgreSQL boot" />
         </Col>
       </Row>
     </Container>
