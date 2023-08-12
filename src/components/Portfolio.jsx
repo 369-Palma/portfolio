6e2fdaeb38f061d4845af9eee8dc92555b6a0a2c
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-08-12 19:13:08 +0200

    portfolio style v.1.1.2

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,9 +10,113 @@ const Portfolio = () => {
 
         {/* FULLSTACK PROJECTS */}
 
-        <div className="containerWorks d-flex justify-content-center p-4">
-          <Row>
+        <div className="containerWorks d-flex flex-column justify-content-space-around">
+          <Row className="containerWorks">
             <h3> FULLSTACK </h3>
+            <Col xs={12} md={6} className="flex-row">
+              <Card className="carta mb-3">
+                <Card.Img variant="top" src={sfondo} />
+                <Card.Body className="bottomCard">
+                  <Card.Title className="dettagli">Art gallery</Card.Title>
+                  <div className="info">
+                    <p className="pt-5">
+                      Showcase of an Apulian artist's gallery
+                    </p>
+                    <p className="mini">
+                      Redux, Javascript, Java, Spring Boot, PostgreSQL
+                    </p>
+
+                    <Link
+                      to="https://github.com/stars/369-Palma/lists/showcase-website-art"
+                      target="_blank"
+                    >
+                      <Button className="bottone">Repo</Button>
+                    </Link>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={6}>
+              <Card className="carta mb-3">
+                <Card.Img variant="top" src={sfondo} />
+                <Card.Body className="bottomCard">
+                  <Card.Title>EasyVacanza</Card.Title>
+                  <div className="info">
+                    <p className="pt-5">
+                      {" "}
+                      Website for searching and booking holidays
+                    </p>
+                    <p className="mini">
+                      React, Javascript, Java, Spring Boot, PostgreSQL
+                    </p>
+
+                    <Link
+                      to="https://github.com/stars/369-Palma/lists/capstone-project"
+                      target="_blank"
+                    >
+                      <Button className="bottone">Repo</Button>
+                    </Link>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+
+          {/* FRONTEND */}
+          <Row className="containerWorks my-5">
+            <h3> FRONTEND </h3>
+            <Col xs={12} md={6} className="flex-row">
+              <Card className="carta mb-3">
+                <Card.Img variant="top" src={sfondo} />
+                <Card.Body className="bottomCard">
+                  <Card.Title className="dettagli">Art gallery</Card.Title>
+                  <div className="info">
+                    <p className="pt-5">
+                      Showcase of an Apulian artist's gallery
+                    </p>
+                    <p className="mini">
+                      Redux, Javascript, Java, Spring Boot, PostgreSQL
+                    </p>
+
+                    <Link
+                      to="https://github.com/stars/369-Palma/lists/showcase-website-art"
+                      target="_blank"
+                    >
+                      <Button className="bottone">Repo</Button>
+                    </Link>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={6}>
+              <Card className="carta mb-3">
+                <Card.Img variant="top" src={sfondo} />
+                <Card.Body className="bottomCard">
+                  <Card.Title>EasyVacanza</Card.Title>
+                  <div className="info">
+                    <p className="pt-5">
+                      {" "}
+                      Website for searching and booking holidays
+                    </p>
+                    <p className="mini">
+                      React, Javascript, Java, Spring Boot, PostgreSQL
+                    </p>
+
+                    <Link
+                      to="https://github.com/stars/369-Palma/lists/capstone-project"
+                      target="_blank"
+                    >
+                      <Button className="bottone">Repo</Button>
+                    </Link>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+
+          {/* BACKEND */}
+          <Row className="containerWorks my-5">
+            <h3> BACKEND </h3>
             <Col xs={12} md={6} className="flex-row">
               <Card className="carta mb-3">
                 <Card.Img variant="top" src={sfondo} />
